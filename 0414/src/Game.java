08af4dd0ff531bad56cfeac0eb6f3c0685ac42c8
leejunseok <junseok825@gmail.com>
2021-04-14 15:49:04 +0900

    30314 이준석(0414)

---
+++ 0414/src/Game.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Game {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("[1:가위 2:바위 3:보]");
+		int user = sc.nextInt();
+		int com = (int)(Math.random()*3+1);
+		System.out.println("-------- 가위 바위 보 !----------");
+		
+		}
+	public static void game(int user, int com){
+		switch(user){
+		case 1:
+			if(user == com){
+				
+			}
+		}
+	}
+		
+
+}
+
+
+
